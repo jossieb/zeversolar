66f5c6c49b7e41d828731ee935bfac5843fce63f
JossieB <jvdhnl@hotmail.com>
2022-11-01 11:21:12 +0100

    Update getZevInv.py

--- getZevInv.py
+++ getZevInv.py
@@ -3,7 +3,7 @@
 # Date:     Nov 2022
 # Function: get produced solar energy levels from ZeverSloar Inverter
 ############################################################################################
-# Version : 0.9
+# Version : 0.91
 ############################################################################################
 
 import urllib.request
@@ -13,6 +13,7 @@ import sqlite3
 import time
 import datetime
 import pytz
+import decimal
 
 # use right time zone
 UTC = pytz.utc
@@ -33,36 +34,44 @@ while True:
     # open URL and check return code
     try:
         res = urllib.request.urlopen(myUrl)
-    except HTTPError as e:
-        mycode = str(e.code)
-        myustatus = "Inverter probably switched off!"
+    except URLError:
+        mycode = "500"
+        myustatus = "Inverter switched off!"
         print("code: " + mycode + " status: " + myustatus)
+        
+        # no response
+        mytimestamp = datetime.datetime.now(CET)
+        myurlstat = mycode + "/" + myustatus    
+        myinverter = ""
+        mytime_dat = ""
+        mystatus = ""
+        mypac = "0" 
+        mycum = "0" 
+        mysolar_stat = "OFF"
         # try again over an hour
         t = 3600
-    except URLError as e:
-        mycode = str(e.code)
-        myustatus = "Site not up!"
-        print("code: " + mycode + " status: " + myustatus)
-        # try again over an hour
-        t = 3600
+        
     else:
         t = 300
         mycode = "200"
         myustatus = ""
-    # response is parsed from inverter string
-    mytimestamp = datetime.datetime.now(CET)
-    myurlstat = mycode + "/" + myustatus
-    
-    strres = res.read(300).decode('utf-8')
-    
-    x = strres.split('\n', 13)
-
-    myinverter = x[2]
-    mytime_dat = x[6] 
-    mystatus = x[7]
-    mypac = x[10] 
-    mycum = x[11]
-    mysolar_stat = x[12]
+        # response is parsed from inverter string
+        mytimestamp = datetime.datetime.now(CET)
+        myurlstat = mycode + "/" + myustatus    
+        strres = res.read(300).decode('utf-8')
+        
+        x = strres.split('\n', 13)
+        myinverter = x[2]
+        mytime_dat = x[6] 
+        mystatus = x[7]
+        mypac = x[10]
+        mycum = x[11]
+        # zeversolar is incorrect with decimals so I have to correct it
+        mycumd = decimal.Decimal(mycum)
+        if mycumd < 1 and len(mycum) == 3:
+            mycum = mycum[:2] + "0" + mycum[2:]
+        mycum = mycum.replace(".", "," )   # decimal comma for local (dutch) usage
+        mysolar_stat = x[12]
     
     # response is stored within database
     con.execute(
